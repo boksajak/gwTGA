316da3fa24662bf04624a4ec233a55e3d22a5c65
boksajak <jakub.boksansky@gmail.com>
2014-10-15 19:36:40 +0200

    Reading of uncompressed images

--- source/gwTGA.h
+++ source/gwTGA.h
@@ -78,10 +78,13 @@ namespace gw {
 		};
 
 		struct TGAImage {
+			// TODO: constructor and destructor - destroy allocated bytes if any
+
 			char* bytes;
 			unsigned int width;
 			unsigned int height;
 			TGAFormat pixelFormat;
+			unsigned char bitsPerPixel;
 		};
 
 		class ITGALoaderListener { 
@@ -89,30 +92,42 @@ namespace gw {
 			virtual char* newTexture(const TGAImage &image) = 0; 
 		}; 
 
+		class TGALoaderListener : public ITGALoaderListener {
+		public: 
+			char* newTexture(const TGAImage &image) {
+				this->image = image;
+				return new char[(image.bitsPerPixel / 8) * (image.height * image.width)];
+			}
+
+			TGAImage image;
+		};
+
 		TGAImage LoadTgaFromFile(char* fileName);
 		TGAImage LoadTga(std::istream &stream);
 
 		void LoadTgaFromFile(char* fileName, ITGALoaderListener* listener);
-		void LoadTga(std::istream stream, ITGALoaderListener* listener);
+		void LoadTga(std::istream &stream, ITGALoaderListener* listener);
 
 		TGAImage LoadTgaFromFile(char* fileName) {
 
 			std::ifstream fileStream;
 			fileStream.open(fileName, std::ifstream::in | std::ifstream::binary);
 
-			TGAImage result = LoadTga(fileStream);
+			// TODO: check file stream for errors
+			TGALoaderListener listener;
+			LoadTga(fileStream, &listener);
 
 			fileStream.close();
 
-			return result;
+			return listener.image;
 		}
 
-		TGAImage LoadTga(std::istream &stream) {
+		void LoadTga(std::istream &stream, ITGALoaderListener* listener) {
 			// TODO: assert the stream is binary and readable
 
 			// TODO: TGA is little endian. Make sure reading from stream is little endian
 
-			TGAImage result;
+			TGAImage resultImage;
 
 			// Read header
 			TGAHeader header;
@@ -130,17 +145,73 @@ namespace gw {
 			stream.read((char*)&header.imageSpec.bitsPerPixel, sizeof(header.imageSpec.bitsPerPixel));
 			stream.read((char*)&header.imageSpec.imgDescriptor, sizeof(header.imageSpec.imgDescriptor));
 
-			result.width = header.imageSpec.width;
-			result.height = header.imageSpec.height;
+			resultImage.width = header.imageSpec.width;
+			resultImage.height = header.imageSpec.height;
+			if (header.colorMapSpec.colorMapLength == 0) {
+				resultImage.bitsPerPixel = header.imageSpec.bitsPerPixel;
+			} else {
+				resultImage.bitsPerPixel = header.colorMapSpec.colorMapEntrySize;
+			}
 			
-
+			// TODO: We do not need pixelFormat anymore
+			switch (resultImage.bitsPerPixel) {
+			case 8:
+				resultImage.pixelFormat = TGAFormat::LUMINANCE_U8;
+				break;
+			case 16:
+				resultImage.pixelFormat = TGAFormat::BGRA5551_U16;
+				break;
+			case 24:
+				resultImage.pixelFormat = TGAFormat::BGR_U24;
+				break;
+			case 32:
+				resultImage.pixelFormat = TGAFormat::BGRA_U32;
+				break;
+			case 96:
+				resultImage.pixelFormat = TGAFormat::BGR_F96;
+				break;
+			default:
+				// TODO: Unknown pixel format
+				break;
+			}
+			
 			// Read image iD - skip this, we do not use image id now
-			if (header.iDLength > 0) {
-				uint8_t* imageId = (uint8_t*) alloca(header.iDLength);
-				stream.read((char*)&imageId, header.iDLength);
+			stream.seekg(header.iDLength, std::ios_base::cur);
+
+			// Read color map
+			char* colorMap = NULL;
+			if (header.colorMapSpec.colorMapLength > 0) {
+				size_t size = header.colorMapSpec.colorMapLength * (header.colorMapSpec.colorMapEntrySize / 8);
+				colorMap = new char[size];
+				if (!colorMap) {
+					// TODO: malloc error
+					/*file.close();
+					return TGALoaderError::MALLOC_ERROR;*/
+					return;
+				}
+				stream.read(colorMap, size);
 			}
 
-			return result;
+			// Read image data
+			size_t pixelsNumber = header.imageSpec.width * header.imageSpec.height;
+			// TODO: Bits per pixel has to be divisible by 8, do some checking before!
+			size_t bytesPerPixel = resultImage.bitsPerPixel / 8;
+			size_t imgDataSize = pixelsNumber * bytesPerPixel;
+
+			// Allocate memory for image data
+			char* imgData = listener->newTexture(resultImage);
+			if (!imgData) {
+					// TODO: malloc error
+				return;
+			}
+
+			// Read pixels
+			if (header.ImageType == 2 || header.ImageType == 3) {
+				// 2 - Uncompressed, RGB images
+				// 3 - Uncompressed, black and white images.
+
+				stream.read(imgData, imgDataSize);
+			}
 		}
 	} 
 }
